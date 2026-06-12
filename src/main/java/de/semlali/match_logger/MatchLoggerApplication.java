c0bda842e92901991813eb0badefcd722a441256
uumqc <uumqc@student.kit.edu>
2026-06-12 17:28:54 +0200

    chore: initial spring boot skeleton

---
+++ src/main/java/de/semlali/match_logger/MatchLoggerApplication.java
@@ -0,0 +1,13 @@
+package de.semlali.match_logger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatchLoggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatchLoggerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/semlali/match_logger/MatchLoggerApplicationTests.java
@@ -0,0 +1,13 @@
+package de.semlali.match_logger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatchLoggerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
